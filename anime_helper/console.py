6bf6fb25bf26e36b8d1ed647c1aac45231a812f4
kliffeup <azatvaleev356778@gmail.com>
2021-12-15 22:50:10 +0300

    Added poetry support, fixed imports

--- anime_helper/console.py
+++ anime_helper/console.py
@@ -1,13 +1,13 @@
-from anime_helper import AnimeHelper
+from anime_helper.anime_helper import AnimeHelper
 from fire import Fire
 
 
 def run():
-    kek = AnimeHelper()
+    anime_helper = AnimeHelper()
 
     Fire({
-        'find-season': kek.get_seasonal_anime,
-        'find-keyword': kek.get_anime_by_keyword,
-        'find-rank': kek.get_anime_ranking,
-        'info': kek.get_anime_info,
+        'find-season': anime_helper.get_seasonal_anime,
+        'find-keyword': anime_helper.get_anime_by_keyword,
+        'find-rank': anime_helper.get_anime_ranking,
+        'info': anime_helper.get_anime_info,
     })
